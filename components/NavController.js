136dd5dc1eec6e37cf8052cea8dbd4a5b57ef5e7
Kim Jun Sik <jinaganda5@gmail.com>
2019-08-11 18:15:42 +0900

    Auth Home

--- components/NavController.js
+++ components/NavController.js
@@ -1,17 +1,13 @@
 import React from "react";
 import { View } from "react-native";
-import {} from "../AuthContext";
+import { useIsLoggedIn } from "../AuthContext";
 import AuthNavigation from "../navigations/AuthNavigation";
 import MainNavigation from "../navigations/MainNavigation";
 
 export default () => {
-  const isLoggedIn = true;
+  const isLoggedIn = useIsLoggedIn();
   return (
-    <View
-      style={{
-        flex: 1
-      }}
-    >
+    <View style={{ flex: 1 }}>
       {isLoggedIn ? <MainNavigation /> : <AuthNavigation />}
     </View>
   );
