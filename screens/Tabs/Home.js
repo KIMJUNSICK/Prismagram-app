bbb5fa63ea1c03589689a97813ac322eac944235
Kim Jun Sik <jinaganda5@gmail.com>
2019-08-13 13:56:10 +0900

    Apollo Context and Receive feed data

--- screens/Tabs/Home.js
+++ screens/Tabs/Home.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import Loader from "../../components/Loader";
+import { gql } from "apollo-boost";
+import { useQuery } from "react-apollo-hooks";
 
 const View = styled.View`
   justify-content: center;
@@ -8,10 +10,38 @@ const View = styled.View`
   flex: 1;
 `;
 
-const Text = styled.Text``;
+const FEED_QUERY = gql`
+  {
+    seeFeed {
+      id
+      location
+      caption
+      user {
+        id
+        avatar
+        userName
+      }
+      files {
+        id
+        url
+      }
+      likeCount
+      isLiked
+      comments {
+        id
+        text
+        user {
+          id
+          userName
+        }
+      }
+      createdAt
+    }
+  }
+`;
 
-export default () => (
-  <View>
-    <Loader />
-  </View>
-);
+export default () => {
+  const { loading, data } = useQuery(FEED_QUERY);
+  console.log(loading, data);
+  return <View>{loading ? <Loader /> : null}</View>;
+};
