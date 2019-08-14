af54946a8496842255b3ef8ccf2a16ffd0ce1b97
Kim Jun Sik <jinaganda5@gmail.com>
2019-08-14 09:54:43 +0900

    Post Swiper and Icons

--- components/Post.js
+++ components/Post.js
@@ -1,9 +1,14 @@
 import React from "react";
-import { Image } from "react-native";
+import { Image, Platform } from "react-native";
+import Swiper from "react-native-swiper";
 import styled from "styled-components";
 import PropTypes from "prop-types";
+import { Ionicons } from "@expo/vector-icons";
+import constants from "../constants";
 
-const Container = styled.View``;
+const Container = styled.View`
+  margin-bottom: 20px;
+`;
 
 const Header = styled.View`
   flex-direction: row;
@@ -19,13 +24,22 @@ const Location = styled.Text`
   font-size: 12px;
 `;
 
+const IconsContainer = styled.View`
+  flex-direction: row;
+  margin-top: 10px;
+`;
+
+const IconContainer = styled.View`
+  margin-left: 10px;
+`;
+
 const Bold = styled.Text`
   font-weight: 500;
 `;
 
 const Touchable = styled.TouchableOpacity``;
 
-const Post = ({ location, user }) => {
+const Post = ({ location, user, files = [] }) => {
   return (
     <Container>
       <Header>
@@ -42,6 +56,43 @@ const Post = ({ location, user }) => {
           </HeaderUserContainer>
         </Touchable>
       </Header>
+      <Swiper
+        style={{ height: constants.height / 2.5 }}
+        paginationStyle={{ position: "absolute", bottom: -25 }}
+        dotStyle={{ width: 4, height: 4 }}
+        activeDotStyle={{ width: 4, height: 4 }}
+      >
+        {files.map(file => (
+          <Image
+            source={{ uri: file.url }}
+            style={{ width: constants.width, height: constants.height / 2.5 }}
+            key={file.id}
+          />
+        ))}
+      </Swiper>
+      <IconsContainer>
+        <Touchable>
+          <IconContainer>
+            <Ionicons
+              size={28}
+              name={
+                Platform.OS === "ios" ? "ios-heart-emtpy " : "md-heart-empty"
+              }
+            />
+          </IconContainer>
+        </Touchable>
+        <Touchable>
+          <IconContainer>
+            <Ionicons
+              size={28}
+              name={
+                Platform.OS === "ios" ? "ios-heart-emtpy " : "md-heart-empty"
+              }
+            />
+          </IconContainer>
+        </Touchable>
+        <IconContainer />
+      </IconsContainer>
     </Container>
   );
 };
